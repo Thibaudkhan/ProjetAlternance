b29f6bf361fc17e007ba3ee4a9c0a964aaa70f55
Unknown <thibaud.roussel@edu.itescia.fr>
2018-12-08 09:37:04 +0100

    animation retour

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>JeChercheUneAlternance</title>
+</head>
+<body>
+	<h1>Ceci est un test </h1>
+	<div class="annimation">
+		<div class="devant"><img src="Danone.png"></div>
+		<div class="derriere"><p>kzrfhbzefkj</p></div>
+	</div>
+
+	<footer>		
+		<div class="blocCopyright">
+		 	Tout droits réservé |  © 2018  Smartacus par Antoine, Thibaud | <a href="#1.php"> Mentions Légales</a> 
+		</div>
+	</footer>
+</body>
+</html>
